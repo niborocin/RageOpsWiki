f828fba082c7a1670c9c7bd4ba22901ec48cdd90
Unknown <p.schiperski@gmail.com>
2018-07-27 00:20:38 +0200

    add: database

---
+++ data.js
@@ -0,0 +1,21 @@
+var data=[
+    {
+        name: "Tom Müller",
+        born: "18.02.2000",
+        vehicle: "hässlicher Golf",
+        bio: "testaskmdapsdmapsdmapsdmapsdmapsodmapsdomapsodmaposdmapsmdapsdmapsodmapsodmapsd",
+
+    },
+    {
+        name: "Justin Bornier",
+        born: "05.09.1999",
+        vehicle: "hässlicher Golf 2.0",
+        bio: "dumeraraskdmnalksdmalskdmalskdmalskdmalskmdlaksmdlaksmdlaksmdlkasmdlkamsdlkamsldkm",
+    },
+    {
+        name: "Tim Hamann",
+        born: "22.08.1999",
+        vehicle: "Corsa mit kaputtem Dach",
+        bio: "stuggiaksldmnaolsdmnaolsndaljsndlajsndlasndlasndlansldkanslkdnalskndlaknalksndlkan",
+    }
+]
