e68e300eb09ae2494207dc0d08b478feaec23133
Unknown <rk@click-solutions.de>
2018-07-27 00:24:11 +0200

    edit: bio, syntax

--- data.js
+++ data.js
@@ -3,19 +3,18 @@ var data=[
         name: "Tom Müller",
         born: "18.02.2000",
         vehicle: "hässlicher Golf",
-        bio: "testaskmdapsdmapsdmapsdmapsdmapsodmapsdomapsodmaposdmapsmdapsdmapsodmapsodmapsd",
-
+        bio: "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
     },
     {
         name: "Justin Bornier",
         born: "05.09.1999",
         vehicle: "hässlicher Golf 2.0",
-        bio: "dumeraraskdmnalksdmalskdmalskdmalskdmalskmdlaksmdlaksmdlaksmdlkasmdlkamsdlkamsldkm",
+        bio: "At vero eos et accusam et justo duo dolores et ea rebum.",
     },
     {
         name: "Tim Hamann",
         born: "22.08.1999",
         vehicle: "Corsa mit kaputtem Dach",
-        bio: "stuggiaksldmnaolsdmnaolsndaljsndlajsndlasndlasndlansldkanslkdnalskndlaknalksndlkan",
+        bio: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
     }
-]
+];
